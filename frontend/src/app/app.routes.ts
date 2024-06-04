f5138caf3c75535bb5391e5165626001af952119
azizx018 <45518976+azizx018@users.noreply.github.com>
2024-06-04 16:24:25 -0500

    added front end dashboard route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,6 @@
-import { Routes } from '@angular/router';
-
+import { Routes } from "@angular/router";
+import { DashboardComponent } from "./features/dashboard/dashboard.component";
 export const routes: Routes = [
-    
-];
+    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    {path: 'dashboard', component: DashboardComponent},
+]
