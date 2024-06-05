2c3a5283ddfca5bbed94d76c4bd3f3e3a4e778c9
azizx018 <45518976+azizx018@users.noreply.github.com>
2024-06-05 13:07:30 -0500

    made a patient controller
    added save and get all patients

--- backend/src/main/java/com/example/patient_data/Controller/PatientController.java
+++ backend/src/main/java/com/example/patient_data/Controller/PatientController.java
@@ -3,26 +3,41 @@ package com.example.patient_data.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.patient_data.PatientRepository.PatientRepository;
+import com.example.patient_data.PatientService.PatientService;
 import com.example.patient_data.entity.Patient;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class PatientController {
 
-    @Autowired
-    PatientRepository patientRepository;
+    private PatientService patientService;
 
+    @Autowired
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+    //gets all patients in database
     @RequestMapping("/dashboard")
-    public List<Patient> Patients()
+    public List<Patient> getAllPatients()
     {
-        return patientRepository.findAll().stream().toList();
+        return patientService.getAllPatients();
         
     }
+    //create a new patient referral
+    @GetMapping("/patient")
+    public ResponseEntity<Patient> savePatient(@RequestBody Patient patient) {
+        Patient newPatient = patientService.savePatient(patient);
+        return ResponseEntity.ok(newPatient);
+    }
+
+
 
 
     
