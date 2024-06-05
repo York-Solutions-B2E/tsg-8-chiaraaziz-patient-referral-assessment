6ae59b96d710e90bf689abdf6cb750aedca0c703
azizx018 <45518976+azizx018@users.noreply.github.com>
2024-06-05 14:19:54 -0500

    get 1 patient works

--- backend/src/main/java/com/example/patient_data/Controller/PatientController.java
+++ backend/src/main/java/com/example/patient_data/Controller/PatientController.java
@@ -1,11 +1,14 @@
 package com.example.patient_data.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,10 +34,17 @@ public class PatientController {
         
     }
     //create a new patient referral
-    @GetMapping("/patient")
+    @PostMapping("/patient")
     public ResponseEntity<Patient> savePatient(@RequestBody Patient patient) {
         Patient newPatient = patientService.savePatient(patient);
         return ResponseEntity.ok(newPatient);
+    }
+
+    //get patients by id
+    @GetMapping("/patients/{id}")
+    public ResponseEntity<Patient> getPatientById(@PathVariable Integer id) {
+        Optional<Patient> patient = patientService.getPatientById(id);
+        return patient.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 
