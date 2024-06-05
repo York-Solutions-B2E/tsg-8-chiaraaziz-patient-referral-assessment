5f5d31f660e49c4ae6e9d7ce3d91a9ad49fdc8fd
azizx018 <45518976+azizx018@users.noreply.github.com>
2024-06-05 14:35:11 -0500

    delete method added

--- backend/src/main/java/com/example/patient_data/Controller/PatientController.java
+++ backend/src/main/java/com/example/patient_data/Controller/PatientController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.DeleteExchange;
 
 import com.example.patient_data.PatientService.PatientService;
 import com.example.patient_data.entity.Patient;
@@ -54,6 +56,12 @@ public class PatientController {
         Patient updatedPatient = patientService.updatedPatient(id, patient);
         return ResponseEntity.ok(updatedPatient);
     }
+
+    //delete a patient
+    @DeleteMapping("/patients/{id}")
+    public ResponseEntity<String> deletePatient(Integer id) {
+        patientService.deletePatient(id);
+        return ResponseEntity.ok("Patient has been deleted");    }
 
 
 
