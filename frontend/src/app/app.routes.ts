0da69f8194a749d0673747241d69ef6fb641c277
azizx018 <45518976+azizx018@users.noreply.github.com>
2024-06-06 15:04:36 -0500

    hooked up new patient button

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from "@angular/router";
 import { DashboardComponent } from "./features/dashboard/dashboard.component";
 import { PatientDetailsComponent } from "./features/patient-details/patient-details.component";
+import { NewPatientComponent } from "./features/new-patient/new-patient.component";
 export const routes: Routes = [
     {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
     {path: 'dashboard', component: DashboardComponent},
-    {path: 'patients/:id', component:PatientDetailsComponent}
+    {path: 'patients/:id', component:PatientDetailsComponent},
+    {path: 'patient', component: NewPatientComponent}
 ]
