f08b36ad3098fcec5f131cee449869454581d15d
azizx018 <45518976+azizx018@users.noreply.github.com>
2024-06-06 14:38:11 -0500

    added pages for new patient and patient details

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from "@angular/router";
 import { DashboardComponent } from "./features/dashboard/dashboard.component";
+import { PatientDetailsComponent } from "./features/patient-details/patient-details.component";
 export const routes: Routes = [
     {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
     {path: 'dashboard', component: DashboardComponent},
+    {path: 'patients/{id}', component:PatientDetailsComponent}
 ]
