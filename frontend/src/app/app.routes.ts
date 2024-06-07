a49e0d4b46f2c12eacffc492f3bb7393a040b86b
azizx018 <45518976+azizx018@users.noreply.github.com>
2024-06-07 13:40:57 -0500

    inital edit page routes ok!

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from "@angular/router";
 import { DashboardComponent } from "./features/dashboard/dashboard.component";
 import { PatientDetailsComponent } from "./features/patient-details/patient-details.component";
 import { NewPatientComponent } from "./features/new-patient/new-patient.component";
+import { EditPatientComponent } from "./features/edit-patient/edit-patient.component";
 export const routes: Routes = [
     {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
     {path: 'dashboard', component: DashboardComponent},
     {path: 'patients/:id', component:PatientDetailsComponent},
-    {path: 'patient', component: NewPatientComponent}
+    {path: 'patient', component: NewPatientComponent},
+    {path: 'edit-patients/:id', component:EditPatientComponent}
 ]
