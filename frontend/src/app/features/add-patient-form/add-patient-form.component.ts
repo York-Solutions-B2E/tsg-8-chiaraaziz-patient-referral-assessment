15cedec21362a7f1888c8cf4371bc2ae934e8a0b
azizx018 <45518976+azizx018@users.noreply.github.com>
2024-06-07 09:07:28 -0500

    add component new patient form

---
+++ frontend/src/app/features/add-patient-form/add-patient-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-patient-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-patient-form.component.html',
+  styleUrl: './add-patient-form.component.css'
+})
+export class AddPatientFormComponent {
+
+}
