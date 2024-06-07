88c9bff3e6bed14f54c82cbd75de6109b47b9852
azizx018 <45518976+azizx018@users.noreply.github.com>
2024-06-07 09:14:24 -0500

    when click on new patient and form page appears

--- frontend/src/app/features/new-patient/new-patient.component.ts
+++ frontend/src/app/features/new-patient/new-patient.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AddPatientFormComponent } from '../add-patient-form/add-patient-form.component';
 
 @Component({
   selector: 'app-new-patient',
   standalone: true,
-  imports: [],
+  imports: [AddPatientFormComponent],
   templateUrl: './new-patient.component.html',
   styleUrl: './new-patient.component.css'
 })
