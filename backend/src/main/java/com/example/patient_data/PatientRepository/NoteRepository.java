2ef2ec63e3ed47378117e20f7c1ef3108718fa03
azizx018 <45518976+azizx018@users.noreply.github.com>
2024-06-11 08:39:44 -0500

    file restructure

--- backend/src/main/java/com/example/patient_data/PatientRepository/NoteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.patient_data.PatientRepository;
-import com.example.patient_data.entity.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoteRepository extends JpaRepository<Patient, Integer> {
-}
